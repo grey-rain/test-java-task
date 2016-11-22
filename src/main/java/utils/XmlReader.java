ded70cf551e26e3e790bb4a1bb25b031df3d36e6
Dariya Yershova <dasha.evil@gmail.com>
2016-11-22 17:49:20 +0300

    XML parser is changed to JDOM

--- src/main/java/utils/XmlReader.java
+++ src/main/java/utils/XmlReader.java
@@ -1,44 +1,44 @@
 package utils;
 
-import org.w3c.dom.Document;
-import org.w3c.dom.Element;
-import org.w3c.dom.Node;
-import org.w3c.dom.NodeList;
+import org.jdom2.Document;
+import org.jdom2.Element;
+import org.jdom2.JDOMException;
+import org.jdom2.input.SAXBuilder;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
-import javax.xml.parsers.DocumentBuilder;
-import javax.xml.parsers.DocumentBuilderFactory;
 import java.io.File;
+import java.io.IOException;
+import java.util.List;
 
 public class XmlReader {
+    private Logger log = LoggerFactory.getLogger(XmlReader.class);
 
     public Object[][] readXml(String filePath) {
+        SAXBuilder builder = new SAXBuilder();
         try {
-            File file = new File(filePath);
-            DocumentBuilderFactory documentBuilderFactory = DocumentBuilderFactory.newInstance();
-            DocumentBuilder documentBuilder = documentBuilderFactory.newDocumentBuilder();
-            Document doc = documentBuilder.parse(file);
-            doc.getDocumentElement().normalize();
-
-            NodeList nodeList = doc.getElementsByTagName("element");
-            int nodesAmount = nodeList.getLength();
-            Object[][] testData = new Object[nodesAmount][6];
-            for (int i = 0; i < nodeList.getLength(); i++) {
-                Node node = nodeList.item(i);
-                if (node.getNodeType() == Node.ELEMENT_NODE) {
-                    //TODO: rework - avoid hardcoded tags and iterator
-                    Element nodeElement = (Element) node;
-                    testData[i][0] = nodeElement.getElementsByTagName("sender").item(0).getTextContent();
-                    testData[i][1] = nodeElement.getElementsByTagName("senderPassword").item(0).getTextContent();
-                    testData[i][2] = nodeElement.getElementsByTagName("subj").item(0).getTextContent();
-                    testData[i][3] = nodeElement.getElementsByTagName("msgBody").item(0).getTextContent();
-                    testData[i][4] = nodeElement.getElementsByTagName("recipient").item(0).getTextContent();
-                    testData[i][5] = nodeElement.getElementsByTagName("recipientPassword").item(0).getTextContent();
+            Document document = builder.build(new File(filePath));
+            Element rootNode = document.getRootElement();
+            List list = rootNode.getChildren("element");
+            int dataSize = list.size();
+            int argumentsSize = ((Element) list.get(0)).getChildren().size();
+            Object[][] elements = new Object[dataSize][argumentsSize];
+            log.debug("DataProvider = [{}][{}]", dataSize, argumentsSize);
+            for (int i = 0; i < dataSize; i++) {
+                Element node = (Element) list.get(i);
+                for (int j = 0; j < argumentsSize; j++) {
+                    elements[i][j] = node.getChildren().get(j).getText();
                 }
             }
-            return testData;
-        } catch (Exception e) {
-            e.printStackTrace();
-            return new Object[][] {};
+            return elements;
+        } catch (IOException io) {
+            System.out.println(io.getMessage());
+            log.error("File not found. {}", io.getLocalizedMessage());
+        } catch (JDOMException jdomex) {
+            System.out.println(jdomex.getMessage());
+            log.error("JDom exception. {}", jdomex.getLocalizedMessage());
         }
+        log.warn("Empty dataProvider will be set.");
+        return new Object[][] {};
     }
 }
